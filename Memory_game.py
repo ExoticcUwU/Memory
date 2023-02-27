27cd140989f025410f13bab0a790ae2195a1c357
dwuerfl <97949138+dwuerfl@users.noreply.github.com>
2023-02-27 17:58:09 +0100

    Add files via upload
    added comments for gui-functions

--- Memory_game.py
+++ Memory_game.py
@@ -21,57 +21,76 @@ from PyQt6.QtWidgets import *
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-    
+        
+        #sets title
         self.setWindowTitle("Memory")
-        self.setFixedSize(QSize(900, 700))    
+        #sets windowsize
+        self.setFixedSize(QSize(900, 700))
+        #sets starting field-size
         self.set_size(1)
         self.safefile = False
         
+        #creates menu bar
         self.menu = self.menuBar()
         self.file_menu =self.menu.addMenu("File")
         self.options_menu = self.menu.addMenu("Options")
         self.size_menu = self.menu.addMenu("Field-Size")
 
         self.save_action=QtGui.QAction("Save",self)
+        #runs the save_score function
         self.save_action.triggered.connect(self.save_score)
         self.file_menu.addAction(self.save_action)
 
         self.load_action=QtGui.QAction("Load", self)
+        #runs the load_game function
         self.load_action.triggered.connect(self.load_game)
         self.file_menu.addAction(self.load_action)
 
         self.quit_action = QtGui.QAction("Quit", self)
+        #closes the window 
         self.quit_action.triggered.connect(QApplication.instance().quit)
         self.options_menu.addAction(self.quit_action)
         
         self.quit_action = QtGui.QAction("Reset", self)
+        #sets field-size back to small
         self.quit_action.triggered.connect(lambda:self.set_size(level=1))
         self.options_menu.addAction(self.quit_action)
         
         self.small_action = QtGui.QAction("Small", self)
+        #sets field-size to small
         self.small_action.triggered.connect (lambda:self.set_size(1, True))
         self.size_menu.addAction(self.small_action)
 
         self.medium_action = QtGui.QAction("Medium", self)
+        #sets field-size to medium
         self.medium_action.triggered.connect(lambda: self.set_size(2, True))
         self.size_menu.addAction(self.medium_action)
 
         self.big_action = QtGui.QAction("Big", self)
+        #sets field-size to big
         self.big_action.triggered.connect(lambda: self.set_size(3, True))
         self.size_menu.addAction(self.big_action)
 
     def set_size(self, level, reset=False):
         self.diff =level
-
+        
+        #defines size and ammount of buttons for small playfield
         if(level == 1):
+            #amount of buttons placed on y-axis
             self.size = 6
+            #amount of buttons placed on x-axis
             self.sizeb = 4
+            #y-size of buttons
             self.button_size = 103
+            #x-size of buttons
             self.button_sizeb = 220
+            #list with the colours used on this playfield-size
             self.color_list = ['Violet', 'Silver', 'Blue', 'Steelblue', 
                                'Crimson', 'Peru', 'Orange', 'Tan', 
                                'Gold', 'Coral', 'Pink', 'Maroon']
             self.memory()
+            
+        #defines size and ammount of buttons for medium playfield    
         elif(level == 2):
             self.size = 6
             self.sizeb = 5
@@ -81,6 +100,8 @@ class MainWindow(QMainWindow):
                                'Peru', 'Orange', 'Tan', 'Gold', 'Coral', 
                                'Pink', 'Maroon', 'Green', 'Beige', 'Indigo']
             self.memory()
+        
+        #defines size and ammount of buttons for big playfield
         elif(level == 3):
             self.size = 7
             self.sizeb = 6
@@ -93,6 +114,8 @@ class MainWindow(QMainWindow):
             self.memory()
 
     def memory(self):
+        
+        #sets layout
         self.Layout = QGridLayout()
         self.level=self.diff
 
@@ -106,22 +129,28 @@ class MainWindow(QMainWindow):
         random.shuffle(self.color_list)
         #creating list for cord of button
         self.digits = []
-
+        
+        #creates a list for buttons
         self.buttons = []
         for i in range(self.size):
             for j in range(self.sizeb):
                 
+                #creates blank button
                 self.button = QPushButton("")
                 #connecting button to a function which a parameter 'button' is passed
                 self.button.clicked.connect(lambda checked, button=self.button: self.show_color(button))
                 self.buttons.append(self.button)
+                #sets size for buttons
                 self.button.setFixedSize(self.button_sizeb, self.button_size)
+                #adds buttons
                 self.Layout.addWidget(self.button,i,j)
-
+        
+        #creates Qt widget for the window
         widget = QWidget()
         widget.setLayout(self.Layout)
         self.setCentralWidget(widget)
-
+        
+        #creates widget for the amount of tries
         self.widget_1 = QLabel("Tries: 0")
         font = self.widget_1.font()
         font.setPointSize(15)
@@ -129,6 +158,7 @@ class MainWindow(QMainWindow):
         self.widget_1.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.Layout.addWidget(self.widget_1,7,0)
         
+        #creates widget for the amount of pairs left
         self.widget_2 = QLabel("Pairs left: %d" % self.current_pairs)
         font = self.widget_2.font()
         font.setPointSize(15)
